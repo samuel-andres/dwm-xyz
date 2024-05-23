37ebd1ed3003d90a3cfcd74bde428073bd19adcf
Samuel Andrés <samuel5848@gmail.com>
2024-05-23 02:31:03 -0300

    KEKW

--- config.h
+++ config.h
@@ -88,7 +88,7 @@ static const Rule rules[] = {
 #include "vanitygaps.c"
 static float mfact                  = 0.55;                     /* factor of master area size [0.05..0.95]                          */
 static int nmaster                  = 1;                        /* number of clients in master area                                 */
-static int resizehints              = 1;                        /* 1 means respect size hints in tiled resizals                     */
+static int resizehints              = 0;                        /* 1 means respect size hints in tiled resizals                     */
 static const int lockfullscreen     = 1;                        /* 1 will force focus on the fullscreen window                      */
 #define FORCE_VSPLIT 1                                          /* nrowgrid layout: force two clients to always split vertically    */
 static const Layout layouts[] = {
